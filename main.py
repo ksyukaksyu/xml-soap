b5f9445facdd378fa4eafe746baa04cb8c0e8f84
admin <k.konovalova@dsec.ru>
2018-02-28 00:06:11 +0300

    extended func 1

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ def get_average_temp(filename):
     with open(filename) as file:
         file_data = file.readlines()
         file_data_list = [x.strip() for x in file_data]
-        print(file_data_list)
-        c = 0
         response = 0
-
         for temp in file_data_list:
             response += client1.service.ConvertTemp(Temperature=temp.split()[0],
                                                     FromUnit='degreeFahrenheit',
                                                     ToUnit='degreeCelsius')
-            c += 1
+        print('Average temp in Celsius: {}'.format(response/len(file_data_list)))
 
-        print(response/c)
-        # или можно сначала посчитать среднее в фаренгейтах, а потом перевести в цельсий
+        # или можно сначала посчитать среднее в фаренгейтах, а потом перевести в цельсий (так менее красиво, но
+        # зато только один запрос к сервису):
+        total_temp = 0
+        for temp in file_data_list:
+            total_temp += int(temp.split()[0])
+        average_temp = total_temp / len(file_data_list)
+        response_2 = client1.service.ConvertTemp(Temperature=average_temp,
+                                                    FromUnit='degreeFahrenheit',
+                                                    ToUnit='degreeCelsius')
+        print(response_2)
 
 
-get_average_temp('./txt/temp.txt')
+
+
+get_average_temp('./txt/temps.txt')
