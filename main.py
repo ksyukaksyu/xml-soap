9fe6e1b71af09852146b11d36600db5d97138898
admin <k.konovalova@dsec.ru>
2018-02-27 23:55:18 +0300

    add func 1

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import osa
 URL = 'http://www.webservicex.net/ConvertTemperature.asmx?WSDL'
 client1 = osa.client.Client(URL)
 
-with open('./txt/temps.txt') as file:
-    file_data = file.readlines()
-    file_data_list = [x.strip() for x in file_data]
-    print(file_data_list)
-    c = 0
-    response = 0
 
-    for temp in file_data_list:
-        response += client1.service.ConvertTemp(Temperature=temp.split()[0],
-                                                FromUnit='degreeFahrenheit',
-                                                ToUnit='degreeCelsius')
-        c += 1
+def get_average_temp(filename):
+    with open(filename) as file:
+        file_data = file.readlines()
+        file_data_list = [x.strip() for x in file_data]
+        print(file_data_list)
+        c = 0
+        response = 0
 
-    print(response/c)
-    # или можно сначала посчитать среднее в фаренгейтах, а потом перевести в цельсий
+        for temp in file_data_list:
+            response += client1.service.ConvertTemp(Temperature=temp.split()[0],
+                                                    FromUnit='degreeFahrenheit',
+                                                    ToUnit='degreeCelsius')
+            c += 1
+
+        print(response/c)
+        # или можно сначала посчитать среднее в фаренгейтах, а потом перевести в цельсий
+
+
+get_average_temp('./txt/temp.txt')
